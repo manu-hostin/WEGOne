8520cdd122d154580008ee90140236c42f9cb34e
Iasmin <iasmin_nunes@estudante.sesisenai.org.br>
2025-04-30 21:11:27 -0300

    SCRUM-41 feat: Implementa o menu

--- Main.java
+++ Main.java
@@ -2,35 +2,58 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Selecione o idioma / Select language / Sprache auswählen:");
-        System.out.println("1 - Português");
-        System.out.println("2 - English");
-        System.out.println("3 - Dutesch");
+		Scanner leia = new Scanner(System.in);
 
-        int escolha = scanner.nextInt();
+		int opcaoMenu = 0;
 
-        if (escolha == 1) {
+		System.out.println("Qual o seu idioma?/ What is your language?/ Was ist Ihre Sprache?");
+		System.out.println("1- Português;");
+		System.out.println("2- English;");
+		System.out.println("3- Deutsch");
+		System.out.println(">");
+		int idioma = leia.nextInt();
 
-            System.out.println("a");
-            System.out.println("a");
-            System.out.println("a");
-            System.out.println("a");
-            System.out.println("a");
-            System.out.println("a ");
-        } else if (escolha == 2) {
+		if (idioma == 1) {
+			System.out.println("Bem-vindo(a) ao WegOne:");
+			System.out.println("");
+			System.out.println("=== MENU ===");
+			System.out.println("1. Cadastrar;");
+			System.out.println("2. Pesquisar;");
+			System.out.println("3. Editar;");
+			System.out.println("4. Excluir;");
+			System.out.println("5. Sair.");
+			System.out.println("Oque você deseja acessar?");
+			System.out.println("> ");
+			opcaoMenu = leia.nextInt();
+			leia.nextLine();
+		} else if (idioma == 2) {
+			System.out.println("Welcome to WegOne:");
+			System.out.println("");
+			System.out.println("=== MENU ===");
+			System.out.println("1. Register;");
+			System.out.println("2. Search;");
+			System.out.println("3. Edit;");
+			System.out.println("4. Delete;");
+			System.out.println("5. Exit.");
+			System.out.println("What do you want to access?");
+			System.out.println("> ");
+			opcaoMenu = leia.nextInt();
+			leia.nextLine();
+		} else if (idioma == 3) {
+			System.out.println("Willkommen bei WegOne:");
+			System.out.println("");
+			System.out.println("=== MENÜ ===");
+			System.out.println("1. Registrieren;");
+			System.out.println("2. Suche;");
+			System.out.println("3. Bearbeiten");
+			System.out.println("4. Löschen;");
+			System.out.println("5. Beenden.");
+			System.out.println("Worauf möchten Sie zugreifen?");
+			System.out.println("> ");
+			opcaoMenu = leia.nextInt();
+			leia.nextLine();
 
-            System.out.println(" __        __   _                            _         ");
-            System.out.println(" \\ \\      / /__| | ___ ___  _ __ ___   ___  | |_ ___   ");
-            System.out.println("  \\ \\ /\\ / / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\ | __/ _ \\ ");
-            System.out.println("   \\ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) | ");
-            System.out.println("    \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___|  \\__\\___/ ");
-        } else if (escolha == 3) {
-            System.out.println("Willkommen!");
-        } else {
-            System.out.println("Opção inválida / Invalid option / Ungültige Option.");
-        }
-
-        scanner.close();
-    }}
+		}
+	}
+}
